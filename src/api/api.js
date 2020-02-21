fde7e1744eabdb352e665b59bf701e390aaa6b9f
Daniel Marin <daniel@DESARROLLO-05.sonartelematics.local>
2020-02-21 17:55:04 -0500

    Add more gets

--- src/api/api.js
+++ src/api/api.js
@@ -5,3 +5,21 @@ export const getVehicles = () => {
 
   return axios.get(url);
 }
+
+export const getPartReviews = () => {
+  const url = 'https://localhost:5001/partreviews';
+
+  return axios.get(url);
+}
+
+export const getVehicleParts = () => {
+  const url = 'https://localhost:5001/vehicleparts';
+
+  return axios.get(url);
+}
+
+export const getEmployees = () => {
+  const url = 'https://localhost:5001/employees';
+
+  return axios.get(url);
+}
