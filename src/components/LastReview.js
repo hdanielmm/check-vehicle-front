5ce28b2822f7403f4375ce0a0ee38087d98c0c3f
hdanielmm <hdanielmm@gmail.com>
2020-03-04 17:01:20 +0000

    Add getemployeeName

--- src/components/LastReview.js
+++ src/components/LastReview.js
@@ -1,16 +1,30 @@
 import React, { useEffect, useState } from 'react'
-import { getVehiclePartReviews } from '../api/api';
+import { getVehiclePartReviews, getEmployeePartReviews } from '../api/api';
 
 const LastReview = ({ vehicle }) => {
   const [lastReview, setLastReview] = useState({
     reviews: []
   });
 
+  const [employeePartReview, setEmployeePartReview] = useState({
+    employees: []
+  })
+
   useEffect(() => {
     const fetchData = async () => {
       const result = await getVehiclePartReviews();
 
       setLastReview({ reviews: [...lastReview.reviews, ...result.data] });
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getEmployeePartReviews();
+
+      setEmployeePartReview({ employees: [...employeePartReview.employees, ...result.data] });
     };
 
     fetchData();
@@ -31,25 +45,33 @@ const LastReview = ({ vehicle }) => {
   }
 
   const lVehicle = lastReview.reviews.filter(filterById);
-
-  const vReview = lVehicle.map(i => i.vehicleReviews[0]);
-
-  const pReview = vReview.map(i => i.partReviews[0]);
+  const vehicleReview = lVehicle.map(i => i.vehicleReviews[0]);
+  const pReview = vehicleReview.map(i => i.partReviews[0]);
+  const eId = pReview.map(i => i.employeeId);
 
   const getDiagnosis = () => {
-    let d = "";
+    let result = "";
     lVehicle.forEach(element => {
       console.log(element.vehicleReviews[0].partReviews[0].diagnosis);
-      d = element.vehicleReviews[0].partReviews[0].diagnosis;
+      result = element.vehicleReviews[0].partReviews[0].diagnosis;
     });
-    return d;
+    return result;
   }
+
+  const getEmployeeName = () => {
+    const employeeObj = employeePartReview.employees.filter(i => eId[0] === i.id);
+    const employeeName = employeeObj.map(e => e.name)[0]
+    return employeeName;
+  }
+
+  console.log('lastReview', lastReview);
+  console.log('employeePartReview', employeePartReview);
 
   const getLastReview = () => {
     return (
       <ul>
         <li>Parte</li>
-        <li>Técnico</li>
+        <li>Técnico: {getEmployeeName()}</li>
         <li>Diagnóstico: {getDiagnosis()}</li>
       </ul>
     )
@@ -58,7 +80,7 @@ const LastReview = ({ vehicle }) => {
   return (
     <div>
       {getLastReview()}
-      
+
     </div>
   )
 }
