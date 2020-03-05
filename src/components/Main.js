cd20b4767db48cdcb26fafa18c2b87be4bc574f8
hdanielmm <hdanielmm@gmail.com>
2020-03-05 22:48:43 +0000

    Delete a comment and and add material_ui

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Vehicle from './Vehicle';
 import Home from './Home';
-// import FindReview from './FindReview';
 import Review from './Review';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const Main = () => (
   <main>
+    <CssBaseline />
     <Switch>
       <Route exact path='/' component={Home} />
       <Route path='/vehicle' component={Vehicle} />
